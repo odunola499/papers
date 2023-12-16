2964c6aecedeb07075f8f859ca13ee215f9e5dad
odunola <jenrola2292@gmail.com>
2023-12-16 21:20:40 +0100

    new

--- multilingual_embeddings/main.py
+++ multilingual_embeddings/main.py
@@ -2,14 +2,19 @@ from utils import load_data
 import os
 from torch import nn
 from transformers import AutoTokenizer, AutoModel
+from torch.utils.data import DataLoader
+import torch
+from transformers.optimization import get_linear_schedule_with_warmup
+from tqdm.auto import tqdm
 
 
 huggingface_key = os.environ['HUGGINGFACE_API_KEY']
 
 dataset = load_data()
+from huggingface_hub import login
 
+login(huggingface_key)
 
-tokenizer = AutoTokenizer.from_pretrained('BAAI/bge-large-en-v1.5')
 model = AutoModel.from_pretrained('BAAI/bge-large-en-v1.5')
                                   
 class Pooler(nn.Module):
@@ -31,31 +36,51 @@ model.pooler = Pooler()
 #now we have our model
 
 #we need to freeze all layers except the pooling module
-
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 for name, param in model.named_parameters():
     if 'pooler' not in name:
         param.__requires_grad = False  #freezing layes
 
 #now we need to finetune the model
 
-dataset = dataset.map(
-    lambda x: tokenizer(
-        x['premise'], max_length = 128, padding = 'max_length',
-        truncation = True
-    ), batched = True
-)
+batch_size = 32
+loader = DataLoader(dataset, batch_size = batch_size)
 
-dataset = dataset.rename_column('input_ids', 'anchor_ids')
-dataset = dataset.rename_column('attention_mask', 'anchor_mask')
+cos_sim = nn.CosineSimilarity().to(device)
+loss_func = nn.CrossEntropyLoss().to(device)
+model.to(device)
+scale = 20.0
+optim = torch.optim.Adam(model.parameters(), lr=2e-5)
+epochs = 2
 
-dataset = dataset.map(
-    lambda x: tokenizer(
-            x['hypothesis'], max_length=128, padding='max_length',
-            truncation=True
-    ), batched=True
-)
 
-dataset = dataset.rename_column('input_ids', 'positive_ids')
-dataset = dataset.rename_column('attention_mask', 'positive_mask')
+for epoch in range(epochs):
+    loop = tqdm(loader, leave = True)
+    for batch in loop:
+        optim.zero_grad()
+        anchor_ids = batch['anchor']['input_ids'].to(device)
+        anchor_mask = batch['anchor']['attention_mask'].to(device)
+        pos_ids = batch['positive']['input_ids'].to(device)
+        pos_mask = batch['positive']['attention_mask'].to(device)
+        a = model(
+            anchor_ids, attention_mask=anchor_mask
+        )[0]  # all token embeddings
+        p = model(
+            pos_ids, attention_mask=pos_mask
+        )[0]
+        scores = torch.stack([
+            cos_sim(
+                a_i.reshape(1, a_i.shape[0]), p
+            ) for a_i in a])
 
-dataset = dataset.remove_columns(['premise', 'hypothesis', 'label', 'token_type_ids'])
+        labels = torch.tensor(range(len(scores)), dtype=torch.long, device=scores.device)
+        # and now calculate the loss
+        loss = loss_func(scores*scale, labels)
+        # using loss, calculate gradients and then optimize
+        loss.backward()
+        optim.step()
+
+
+
+model.push_to_hub('odunola/baai_768')
+
--- multilingual_embeddings/utils.py
+++ multilingual_embeddings/utils.py
@@ -1,4 +1,6 @@
 import datasets
+from transformers import AutoTokenizer
+tokenizer = AutoTokenizer.from_pretrained('BAAI/bge-large-en-v1.5')
 
 
 
@@ -11,8 +13,31 @@ def load_data():
     dataset = datasets.concatenate_datasets([snli, mnli])
     del snli, mnli
     dataset = dataset.filter(
-        lambda x: True iff x['label'] == 0 else False
+        lambda x: True if x['label'] == 0 else False
     )
+
+    dataset = dataset.map(
+    lambda x: tokenizer(
+        x['premise'], max_length = 128, padding = 'max_length',
+        truncation = True
+    ), batched = True
+    )
+
+    dataset = dataset.rename_column('input_ids', 'anchor_ids')
+    dataset = dataset.rename_column('attention_mask', 'anchor_mask')
+
+    dataset = dataset.map(
+        lambda x: tokenizer(
+                x['hypothesis'], max_length=128, padding='max_length',
+                truncation=True
+        ), batched=True
+    )
+
+    dataset = dataset.rename_column('input_ids', 'positive_ids')
+    dataset = dataset.rename_column('attention_mask', 'positive_mask')
+
+    dataset = dataset.remove_columns(['premise', 'hypothesis', 'label', 'token_type_ids'])
+    dataset.set_format(type='torch', output_all_columns=True)
     return dataset
 
 
