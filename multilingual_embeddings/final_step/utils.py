929bc4733b0255bb11234aad0d97135c3ce3bb29
odunola <jenrola2292@gmail.com>
2023-12-22 23:15:59 +0100

    added final touches

--- multilingual_embeddings/final_step/utils.py
+++ multilingual_embeddings/final_step/utils.py
@@ -3,14 +3,24 @@ from transformers import AutoTokenizer
 import datasets
 tokenizer = AutoTokenizer.from_pretrained('BAAI/bge-large-en-v1.5')
 
+def remove_nonetypes(pairs):
+    eng = pairs['eng']
+    yor = pairs['yor']
+    good_eng, good_yor = [], []
+    for i , j in zip(eng, yor):
+        if (len(i) < 4 or len(j) < 4) or (i == None or j == None):
+            continue
+        good_eng.append(i)
+        good_yor.append(j)
+    return {'eng': good_eng, 'yor':good_yor}
 
 def load_data_pairs():
     dataset_url = 'odunola/yoruba-english-pairs'
     dataset = load_dataset(dataset_url, split = 'train')
-
+    dataset = dataset.map(lambda x: remove_nonetypes(x), batched = True).shuffle().select(range(150000))
     dataset = dataset.map(
         lambda x: tokenizer(
-            x['eng'], max_length = 128, padding = 'max_length', truncation = True
+            x['eng'], max_length = 256, padding = 'max_length', truncation = True
         ), batched = True
     )
     dataset = dataset.rename_column('input_ids', 'positive_ids')
@@ -40,20 +50,21 @@ def load_data_embs():
     dataset = dataset.filter(
         lambda x: True if x['label'] == 0 else False
     )
-
+    dataset = dataset().shuffle().select(range(150000))
+    
     dataset = dataset.map(
-    lambda x: tokenizer(
-        x['premise'], max_length = 128, padding = 'max_length',
-        truncation = True
-    ), batched = True
-    )
+            lambda x: tokenizer(
+                x['premise'], max_length = 256, padding = 'max_length',
+                truncation = True
+            ), batched = True
+            )
 
     dataset = dataset.rename_column('input_ids', 'anchor_ids')
     dataset = dataset.rename_column('attention_mask', 'anchor_mask')
     dataset = dataset.rename_column('token_type_ids', 'anchor_token_ids')
     dataset = dataset.map(
         lambda x: tokenizer(
-                x['hypothesis'], max_length=128, padding='max_length',
+                x['hypothesis'], max_length=256, padding='max_length',
                 truncation=True
         ), batched=True
     )
@@ -68,7 +79,7 @@ def load_data_embs():
 
 
 def load_data():
-    data_1, data_2 = load_data_pairs().shuffle().select(range(150000)), load_data_embs().shuffle().select(range(150000))
+    data_1, data_2 = load_data_pairs(), load_data_embs()
     dataset = datasets.concatenate_datasets([data_1, data_2])
     dataset.set_format(type='torch', output_all_columns=True)
     return dataset.shuffle()
