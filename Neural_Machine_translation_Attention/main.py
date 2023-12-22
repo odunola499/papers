9fa685041239c57018f98dfc575dfb6e5bb96aec
odunola <jenrola2292@gmail.com>
2023-12-22 06:58:39 +0100

    added neural_machine_translation_with_rnns_and_attention

---
+++ Neural_Machine_translation_Attention/main.py
@@ -0,0 +1,128 @@
+from utils import load_data, tokenizer
+import torch
+from torch import nn
+from transformers import AutoTokenizer, AutoModel
+from torch.utils.data import DataLoader
+from torch.optim import AdamW
+from torch.nn import CrossEntropyLoss
+from tqdm.auto import tqdm
+#first we will use a new tokenizer
+
+vocab_size = tokenizer.vocab_size
+embedding_dim = 384
+units = 128
+train_data, valid_data, test_data = load_data(subset = True)
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+target_embedder = nn.Embedding(tokenizer.vocab_size,units)
+
+class Encoder(nn.Module):
+    def __init__(self, embedding_dim = embedding_dim):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.rnn = nn.GRU(embedding_dim, units, bidirectional = True, batch_first = True)
+
+    def forward(self, input_ids):
+        x = self.embedding(input_ids)
+        x,_ = self.rnn(x)
+        x = x[:,:,units:] + x[:,:,:units]
+        return x
+
+#value is context, 
+#attention
+class CrossAttention(nn.Module):
+    def __init__(self, units = units):
+        super().__init__()
+        self.mha = nn.MultiheadAttention(units, num_heads=1, batch_first = True)
+        self.layernorm = nn.LayerNorm(units)
+    def forward(self, input_ids, context):
+        attention_output, attention_scores = self.mha(query = input_ids, value = context, key = context)
+        x = torch.add(input_ids, attention_output)
+        x = self.layernorm(x)
+        return x
+
+#decoder
+class Decoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.rnn = nn.GRU(embedding_dim,units)
+        self.attention = CrossAttention(units)
+        self.fc = nn.Linear(units, vocab_size)
+    def forward(self, context, targ_in, state = None):
+        x = self.embedding(targ_in)
+
+        x, state = self.rnn(x, state)
+
+        x = self.attention(x, context)
+        logits = self.fc(x)
+
+        return logits
+
+
+class Translator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+        #now we write training loop
+    
+    def forward(self, inputs):
+        context, x = inputs
+        context = self.encoder(context)
+        logits = self.decoder(context, x)
+
+        return logits
+    
+
+model = Translator()
+model.to(device)
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+total_params = count_parameters(model)
+print(f"Total trainable parameters: {total_params}")
+
+
+optimizer = AdamW(model.parameters())
+loss_func = CrossEntropyLoss()
+epochs = 20
+#batch_size = input('input batch size: ')
+batch_size = 32
+train_loader = DataLoader(train_data, batch_size = batch_size)
+valid_loader = DataLoader(valid_data, batch_size = batch_size)
+test_loader = DataLoader(test_data, batch_size = batch_size)
+
+
+print(f"Total trainable parameters: {total_params}")
+for epoch in range(epochs):
+    train_loss = 0.0
+    model.train()
+    for batch in tqdm(train_loader):
+        context_ids = batch['en_input_ids'].to(device)
+        target_in_ids = batch['es_input_ids'].to(device)
+        target_out_ids = batch['es_target_ids'].to(device)
+        optimizer.zero_grad()
+        output = model(context_ids, target_in_ids)
+        loss = loss_func(output.view(-1, tokenizer.vocab_size),target_out_ids.view(-1))
+        train_loss += loss.item()
+        loss.backward()
+        optimizer.step()
+    train_loss /= len(train_loader)
+
+    eval_loss = 0.0
+    model.eval()
+    for batch in tqdm(valid_loader):
+        context_ids = batch['en_input_ids'].to(device)
+        target_in_ids = batch['es_input_ids'].to(device)
+        target_out_ids = batch['es_target_ids'].to(device)
+        with torch.no_grad():
+            output = model(context_ids, target_in_ids)
+            loss = loss_func(output.view(-1, tokenizer.vocab_size),target_out_ids.view(-1))
+        eval_loss += loss.item()
+    eval_loss /= len(valid_loader)
+
+    print(f"after epoch {epoch + 1}: Training Loss is {train_loss}")
+    print(f"after epoch {epoch + 1}: Training Loss is {eval_loss}")
+
+    
---
+++ Neural_Machine_translation_Attention/utils.py
@@ -0,0 +1,87 @@
+#code here retrieves preprocessed data
+from datasets import load_dataset
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
+#english to spanish
+max_length = 384
+def get_es_input_target(batch, max_length=max_length):
+    # Tokenize the Spanish translation with no padding/truncation
+    tokens = tokenizer([i['es'] for i in batch['translation']], add_special_tokens=False)
+    ids = tokens['input_ids']
+
+    new_ids_target, new_masks_target, new_token_ids_target = [], [], []
+    for id in ids:
+        new_id = id
+        length = len(new_id)
+        extra_length = max_length - length
+        new_id = new_id + [tokenizer.pad_token_id]* extra_length
+        assert len(new_id) == max_length
+        new_ids_target.append(new_id)
+        new_mask = [1]* length + [0]* extra_length
+        new_token_id = [0] * max_length
+        new_masks_target.append(new_mask)
+        new_token_ids_target.append(new_token_id)
+        
+    new_ids_input, new_masks_input, new_token_ids_input = [], [], [] 
+    for id in ids:
+        new_id = id[:-1]
+        new_id = [tokenizer.bos_token_id] + new_id
+        length = len(new_id)
+        extra_length = max_length - length
+        new_id = new_id + [tokenizer.pad_token_id]* extra_length
+        assert len(new_id) == max_length
+        new_ids_input.append(new_id)
+        new_mask = [1]* length + [0]* extra_length
+        new_token_id = [0] * max_length
+        new_masks_input.append(new_mask)
+        new_token_ids_input.append(new_token_id)
+    return {
+        'es_input_ids' : new_ids_input,
+        'es_mask_input' : new_masks_input,
+        'es_token_ids_input': new_token_ids_input,
+        'es_target_ids': new_ids_target,
+        'es_masks_target': new_masks_target,
+        'es_token_ids_target': new_token_ids_target
+    }
+
+
+
+
+
+
+
+def preprocess_target_out(batch):
+    texts = [i['es'] for i in batch['translation']]
+
+def preprocess_data(dataset, max_length = max_length):
+    dataset = dataset.map(
+        lambda x: tokenizer([i['en'] for i in x['translation']], max_length = max_length, padding = 'max_length', truncation = True), batched = True
+    )
+    dataset = dataset.rename_column('input_ids', 'en_input_ids')
+    dataset = dataset.rename_column('attention_mask', 'en_attention_mask')
+    dataset = dataset.rename_column('token_type_ids', 'en_token_type_ids')
+
+    dataset = dataset.map(
+       lambda x: get_es_input_target(x), batched = True
+    )
+
+    dataset = dataset.remove_columns(['translation'])
+    dataset.set_format(type = 'torch', output_all_columns = True)
+    return dataset
+
+def load_data(subset = True):
+    if subset:
+        dataset = load_dataset('opus100','en-es')['validation']
+    else:
+        dataset = load_dataset('opus100','en-es')['train']
+    #next we split the data
+    splits = dataset.train_test_split(test_size = 0.3)
+    train_data = splits['train']
+    valid_test_data = splits['test'].train_test_split(test_size = 0.2)
+    valid_data = valid_test_data['train']
+    test_data = valid_test_data['test']
+    train_data = preprocess_data(train_data)
+    valid_data = preprocess_data(valid_data)
+    test_data = preprocess_data(test_data)
+    return train_data, valid_data, test_data
