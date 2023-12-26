4e6fd3586816ef9fc51270cc9b8a9d673801ebe6
odunola <jenrola2292@gmail.com>
2023-12-26 18:31:56 +0100

    fixed bug

--- Learning Sound/utils.py
+++ Learning Sound/utils.py
@@ -1,10 +1,8 @@
 from datasets import load_dataset
-from huggingface_hub import login
-import os
 from transformers import WhisperProcessor
 import torch
 from typing import Any, Dict, List, Union
-
+import evaluate
 
 def process_data(batch, feature_extractor, tokenizer):
     audio = batch["audio"]
@@ -20,28 +18,21 @@ def load_data():
     data = load_dataset(data_url)
     feature_extractor = processor.feature_extractor
     tokenizer = processor.tokenizer
-    data = data.map(lambda batch: process_data(batch, feature_extractor, tokenizer))
+    data = data.map(lambda batch: process_data(batch, feature_extractor, tokenizer), batched=True)
     data = data.train_test_split(test_size = 0.2)
     train_data = data['train']
     valid_data = data['test']
     return train_data, valid_data
 
 class DataCollator:
-    processor: Any
+    def __init__(self, processor : Any):
+        self.processor = processor 
     def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
         input_features = [{"input_features": feature["input_features"]} for feature in features]
         batch = self.processor.feature_extractor.pad(input_features, return_tensors="pt")
-
-        # get the tokenized label sequences
         label_features = [{"input_ids": feature["labels"]} for feature in features]
-        # pad the labels to max length
         labels_batch = self.processor.tokenizer.pad(label_features, return_tensors="pt")
-
-        # replace padding with -100 to ignore loss correctly
         labels = labels_batch["input_ids"].masked_fill(labels_batch.attention_mask.ne(1), -100)
-
-        # if bos token is appended in previous tokenization step,
-        # cut bos token here as it's append later anyways
         if (labels[:, 0] == self.processor.tokenizer.bos_token_id).all().cpu().item():
             labels = labels[:, 1:]
 
@@ -49,8 +40,19 @@ class DataCollator:
         return batch
     
 
-login(os.environ['HUGGINGFACE_API_KEY'])
+
 model_url = 'openai/whisper-small'
 data_url = 'odunola/yoruba_audio_data'
 processor = WhisperProcessor.from_pretrained(model_url, language = 'Yoruba')
 data_collator = DataCollator(processor = processor)
+metric = evaluate.load("wer")
+
+def compute_metrics(pred):
+    pred_ids = pred.predictions
+    label_ids = pred.label_ids
+    label_ids[label_ids == -100] = processor.tokenizer.pad_token_id
+    pred_str = processor.tokenizer.batch_decode(pred_ids, skip_special_tokens = True)
+    label_str = processor.tokenizer.batch_decode(label_ids, skip_special_tokens = True)
+
+    wer = 100 * metric.compute(predictions=pred_str, references = label_str)
+    return {'wer': wer}
