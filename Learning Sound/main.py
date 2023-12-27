b82e2fc0b872a425285d9c71c4f65df85f9aab15
odunola <jenrola2292@gmail.com>
2023-12-27 08:02:57 +0100

    fixed bug

--- Learning Sound/main.py
+++ Learning Sound/main.py
@@ -1,11 +1,15 @@
 import os
 from utils import model_url, data_collator, compute_metrics, load_data, processor
-from transformers import WhisperForConditionalGeneration, Seq2SeqTrainingArguments, Seq2SeqTrainer
+from transformers import (
+    WhisperForConditionalGeneration,
+    Seq2SeqTrainingArguments,
+    Seq2SeqTrainer,
+)
 from huggingface_hub import login
 import wandb
 
-wandb.login(key = os.environ['WANDB_API_KEY'])
-login(token = os.environ['HUGGINGFACE_API_KEY'])
+wandb.login(key=os.environ["WANDB_API_KEY"])
+login(token=os.environ["HUGGINGFACE_API_KEY"])
 model = WhisperForConditionalGeneration.from_pretrained(model_url)
 model.config.forced_decoder_ids = None
 model.config.suppress_tokens = []
@@ -14,36 +18,36 @@ collator = data_collator
 train_data, valid_data = load_data()
 
 training_args = Seq2SeqTrainingArguments(
-    output_dir = "./yoruba_whisper",
+    output_dir="./yoruba_whisper",
     per_device_train_batch_size=batch_size,
     gradient_accumulation_steps=1,
-    learning_rate = 1e-5,
-    warmup_steps = 500,
-    max_steps = 4000,
+    learning_rate=1e-5,
+    warmup_steps=500,
+    max_steps=4000,
     gradient_checkpointing=True,
-    fp16 = True,
-    evaluation_strategy = 'steps',
+    fp16=True,
+    evaluation_strategy="steps",
     per_device_eval_batch_size=batch_size,
     predict_with_generate=True,
     generation_max_length=255,
     save_steps=1000,
-    eval_steps = 1000,
-    logging_steps = 25,
-    report_to = ['tensorboard', 'wandb'],
+    eval_steps=1000,
+    logging_steps=25,
+    report_to=["tensorboard", "wandb"],
     load_best_model_at_end=True,
-    metric_for_best_model= 'wer',
-    greater_is_better = True,
-    push_to_hub = True
+    metric_for_best_model="wer",
+    greater_is_better=True,
+    push_to_hub=True,
 )
 
 trainer = Seq2SeqTrainer(
-    args = training_args,
-    model = model,
-    train_dataset = train_data,
-    eval_dataset = valid_data,
-    data_collator = data_collator,
-    compute_metrics = compute_metrics,
-    tokenizer = processor.feature_extractor
+    args=training_args,
+    model=model,
+    train_dataset=train_data,
+    eval_dataset=valid_data,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+    tokenizer=processor.feature_extractor,
 )
 
-trainer.push_to_hub('training complete')
+trainer.push_to_hub("training complete")
--- Learning Sound/utils.py
+++ Learning Sound/utils.py
@@ -4,55 +4,62 @@ import torch
 from typing import Any, Dict, List, Union
 import evaluate
 
+
 def process_data(batch, feature_extractor, tokenizer):
     audio = batch["audio"]
-    sentences = batch['sentence']
-    input_features = feature_extractor(audio["array"], sampling_rate = audio['sampling_rate']).input_features[0]
+    sentences = batch["sentence"]
+    input_features = feature_extractor(
+        audio["array"], sampling_rate=audio["sampling_rate"]
+    ).input_features[0]
     labels = tokenizer(sentences).input_ids
-    return {
-        "input_features":input_features,
-        "labels": labels
-    }
+    return {"input_features": input_features, "labels": labels}
+
 
 def load_data():
-    data = load_dataset(data_url)
-    feature_extractor = processor.feature_extractor
-    tokenizer = processor.tokenizer
-    data = data.map(lambda batch: process_data(batch, feature_extractor, tokenizer))
-    data = data.train_test_split(test_size = 0.2)
-    train_data = data['train']
-    valid_data = data['test']
+    data = load_dataset(data_url)['train']
+    data = data.train_test_split(test_size=0.2)
+    train_data = data["train"]
+    valid_data = data["test"]
     return train_data, valid_data
 
+
 class DataCollator:
-    def __init__(self, processor : Any):
-        self.processor = processor 
-    def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
-        input_features = [{"input_features": feature["input_features"]} for feature in features]
-        batch = self.processor.feature_extractor.pad(input_features, return_tensors="pt")
+    def __init__(self, processor: Any):
+        self.processor = processor
+
+    def __call__(
+        self, features: List[Dict[str, Union[List[int], torch.Tensor]]]
+    ) -> Dict[str, torch.Tensor]:
+        input_features = [
+            {"input_features": feature["input_features"]} for feature in features
+        ]
+        batch = self.processor.feature_extractor.pad(
+            input_features, return_tensors="pt"
+        )
         label_features = [{"input_ids": feature["labels"]} for feature in features]
         labels_batch = self.processor.tokenizer.pad(label_features, return_tensors="pt")
-        labels = labels_batch["input_ids"].masked_fill(labels_batch.attention_mask.ne(1), -100)
+        labels = labels_batch["input_ids"].masked_fill(
+            labels_batch.attention_mask.ne(1), -100
+        )
         if (labels[:, 0] == self.processor.tokenizer.bos_token_id).all().cpu().item():
             labels = labels[:, 1:]
-
         batch["labels"] = labels
         return batch
-    
 
 
-model_url = 'openai/whisper-small'
-data_url = 'odunola/yoruba_audio_data'
-processor = WhisperProcessor.from_pretrained(model_url, language = 'Yoruba')
-data_collator = DataCollator(processor = processor)
+model_url = "openai/whisper-small"
+data_url = "odunola/yoruba_audio_preprocessed"
+processor = WhisperProcessor.from_pretrained(model_url, language="Yoruba")
+data_collator = DataCollator(processor=processor)
 metric = evaluate.load("wer")
+
 
 def compute_metrics(pred):
     pred_ids = pred.predictions
     label_ids = pred.label_ids
     label_ids[label_ids == -100] = processor.tokenizer.pad_token_id
-    pred_str = processor.tokenizer.batch_decode(pred_ids, skip_special_tokens = True)
-    label_str = processor.tokenizer.batch_decode(label_ids, skip_special_tokens = True)
+    pred_str = processor.tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
+    label_str = processor.tokenizer.batch_decode(label_ids, skip_special_tokens=True)
 
-    wer = 100 * metric.compute(predictions=pred_str, references = label_str)
-    return {'wer': wer}
+    wer = 100 * metric.compute(predictions=pred_str, references=label_str)
+    return {"wer": wer}
