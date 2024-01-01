d4ca0e0fb6602a62fa8a2add693fa8dd80243e62
odunola <jenrola2292@gmail.com>
2024-01-01 18:55:53 +0100

    new traning

---
+++ french_whisper/main.py
@@ -0,0 +1,54 @@
+import os
+from utils import model_url, data_collator, compute_metrics, load_data, processor
+from transformers import (
+    WhisperForConditionalGeneration,
+    Seq2SeqTrainingArguments,
+    Seq2SeqTrainer,
+)
+from huggingface_hub import login
+import wandb
+
+wandb.login(key=os.environ["WANDB_API_KEY"])
+login(token=os.environ["HUGGINGFACE_API_KEY"])
+model = WhisperForConditionalGeneration.from_pretrained(model_url)
+model.config.forced_decoder_ids = None
+model.config.suppress_tokens = []
+batch_size = 16
+collator = data_collator
+train_data, valid_data = load_data()
+
+training_args = Seq2SeqTrainingArguments(
+    output_dir="./french_whisper_translate",
+    per_device_train_batch_size=batch_size,
+    gradient_accumulation_steps=1,
+    learning_rate=1e-5,
+    warmup_steps=500,
+    max_steps=3000,
+    gradient_checkpointing=True,
+    fp16=True,
+    evaluation_strategy="steps",
+    per_device_eval_batch_size=batch_size,
+    predict_with_generate=True,
+    generation_max_length=255,
+    save_steps=1000,
+    eval_steps=1000,
+    logging_steps=25,
+    report_to=["tensorboard", "wandb"],
+    load_best_model_at_end=True,
+    metric_for_best_model="wer",
+    greater_is_better=True,
+    push_to_hub=True,
+)
+
+trainer = Seq2SeqTrainer(
+    args=training_args,
+    model=model,
+    train_dataset=train_data,
+    eval_dataset=valid_data,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+    tokenizer=processor.feature_extractor,
+)
+
+trainer.train()
+trainer.push_to_hub("training complete")
---
+++ french_whisper/utils.py
@@ -0,0 +1,68 @@
+from datasets import load_dataset
+from transformers import WhisperProcessor
+import torch
+from typing import Any, Dict, List, Union
+import evaluate
+
+
+# will continue in free time. want to learn about distilling whisper
+def process_data(batch, feature_extractor, tokenizer):
+    audio = batch["audio"]
+    sentences = batch["sentence"]
+    input_features = feature_extractor(
+        audio["array"], sampling_rate=audio["sampling_rate"]
+    ).input_features[0]
+    labels = tokenizer(sentences).input_ids
+    return {"input_features": input_features, "labels": labels}
+
+
+def load_data():
+    data = load_dataset(data_url)["train"].shuffle()
+    data = data.train_test_split()
+    train_data = data["train"]
+    valid_data = data["test"]
+    return train_data, valid_data
+
+
+class DataCollator:
+    def __init__(self, processor: Any):
+        self.processor = processor
+
+    def __call__(
+        self, features: List[Dict[str, Union[List[int], torch.Tensor]]]
+    ) -> Dict[str, torch.Tensor]:
+        input_features = [
+            {"input_features": feature["input_features"]} for feature in features
+        ]
+        batch = self.processor.feature_extractor.pad(
+            input_features, return_tensors="pt"
+        )
+        label_features = [{"input_ids": feature["labels"]} for feature in features]
+        labels_batch = self.processor.tokenizer.pad(label_features, return_tensors="pt")
+        labels = labels_batch["input_ids"].masked_fill(
+            labels_batch.attention_mask.ne(1), -100
+        )
+        if (labels[:, 0] == self.processor.tokenizer.bos_token_id).all().cpu().item():
+            labels = labels[:, 1:]
+        batch["labels"] = labels
+        return batch
+
+
+model_url = "openai/whisper-tiny"
+data_url = "odunola/french-audio-preprocessed"
+processor = WhisperProcessor.from_pretrained(
+    model_url, language="French", task="translate"
+)
+data_collator = DataCollator(processor=processor)
+metric = evaluate.load("wer")
+
+
+def compute_metrics(pred):
+    pred_ids = pred.predictions
+    label_ids = pred.label_ids
+    label_ids[label_ids == -100] = processor.tokenizer.pad_token_id
+    pred_str = processor.tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
+    label_str = processor.tokenizer.batch_decode(label_ids, skip_special_tokens=True)
+
+    wer = 100 * metric.compute(predictions=pred_str, references=label_str)
+    return {"wer": wer}
